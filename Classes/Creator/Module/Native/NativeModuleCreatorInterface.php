d37d4a83e62249f2f62fb1266755c7b410e56e27
Stefan Frömken <froemken@gmail.com>
2025-09-09 19:03:32 +0200

    [FEATURE] Add module command (#94)
    * [WIP][FEATURE] Add module command
    
    * Add argument names to ModuleInformation constructor
    
    * [TASK] Rebase
    
    * [TASK] Add tests
    
    Add info about non-extbase modules not yet supported
    
    * [TASK] Apply CGL
    
    * Rebase
    
    ---------
    
    Co-authored-by: lina.wolf <lwolf@w-commerce.de>

---
+++ Classes/Creator/Module/Native/NativeModuleCreatorInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the package friendsoftypo3/kickstarter.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+namespace FriendsOfTYPO3\Kickstarter\Creator\Module\Native;
+
+use FriendsOfTYPO3\Kickstarter\Information\ModuleInformation;
+use Symfony\Component\DependencyInjection\Attribute\AutoconfigureTag;
+
+#[AutoconfigureTag('ext-kickstarter.creator.module.native')]
+interface NativeModuleCreatorInterface
+{
+    public function create(ModuleInformation $moduleInformation): void;
+}
